479de04eb8d25334caae7cccc1be3084665be031
Dany Sluijk <dany@atlasdev.nl>
2019-10-17 13:02:55 +0200

    feat: style the header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -7,8 +7,9 @@ export default function Header(props: {}): JSX.Element {
     <header class={style.header}>
       <img src="../../assets/face.png" alt="Headshot" class={style.image} />
       <div class={style.profile}>
-        <h1>Hi, I'm Dany Sluijk</h1>
-        profile thing
+        <h1>Hi, I'm Dany.</h1>
+        <h2>Bachelor student Computer Science &amp; Engineering</h2>
+        <p>I like to simplify and speed up the web.</p>
       </div>
     </header>
   );
