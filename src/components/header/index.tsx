501597eb3bd5ca60ea61ac34c631f529d3640d73
Dany Sluijk <dany@atlasdev.nl>
2019-10-18 17:10:56 +0200

    fix: fix the mobile layout

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -14,7 +14,9 @@ import * as style from "./style.scss";
 export default function Header(props: {}): JSX.Element {
   return (
     <header class={style.header}>
-      <img src="../../assets/face.png" alt="Headshot" class={style.image} />
+      <div class={style.image}>
+        <img src="../../assets/face.png" alt="Headshot" />
+      </div>
       <div class={style.profile}>
         <h1>Hi, I'm Dany.</h1>
         <h2>Bachelor student Computer Science &amp; Engineering</h2>
